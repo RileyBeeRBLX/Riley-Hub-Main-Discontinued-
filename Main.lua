6ef72f37e51f28ee389de22ba3520a5297302c2a
RileyBeeRBLX <156512500+RileyBeeRBLX@users.noreply.github.com>
2024-01-22 21:43:44 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,49 +1,143 @@
-local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/ionlyusegithubformcmods/1-Line-Scripts/main/Mobile%20Friendly%20Orion')))()
+local Window = OrionLib:MakeWindow({Name = "Riley Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
 
+--[[
+Name = <string> - The name of the UI.
+HidePremium = <bool> - Whether or not the user details shows Premium status or not.
+SaveConfig = <bool> - Toggles the config saving in the UI.
+ConfigFolder = <string> - The name of the folder where the configs are saved.
+IntroEnabled = <bool> - Whether or not to show the intro animation.
+IntroText = <string> - Text to show in the intro animation.
+IntroIcon = <string> - URL to the image you want to use in the intro animation.
+Icon = <string> - URL to the image you want displayed on the window.
+CloseCallback = <function> - Function to execute when the window is closed.
+]]
+local Tab = Window:MakeTab({
+	Name = "Main Stuff",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
 
-local MainWindow = Rayfield:CreateWindow({
-	Name = "Riley Hub",
-	LoadingTitle = "Riley Hub Is Loading!",
-	LoadingSubtitle = "by Riley",
-	ConfigurationSaving = {
-	   Enabled = true,
-	   FolderName = nil, -- Create a custom folder for your hub/game
-	   FileName = "Riley Hub"
-	},
-	Discord = {
-	   Enabled = false,
-	   Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD.
-	   RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-	},
-	KeySystem = false, -- Set this to true to use our key system
-	KeySettings = {
-	   Title = "Riley Hub",
-	   Subtitle = "Epic Riley Key System",
-	   Note = "Uhh How Did You Get This Script When Its On Testing!",
-	   FileName = "Riley Key",
-	   SaveKey = true,
-	   GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-	   Key = "key"
-	}
- })
-
-
- local MainTab = MainWindow:CreateTab("Main", 4483362458) -- Title, Image
-
-
- local Button = MainTab:CreateButton({
+--[[
+Name = <string> - The name of the tab.
+Icon = <string> - The icon of the tab.
+PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
+]]
+Tab:AddButton({
 	Name = "KToll Mimic Script",
 	Callback = function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/KTollT/KTollT/main/README.md'))()
-	end,
- })
+      		print("button pressed")
+  	end    
+})
 
+--[[
+Name = <string> - The name of the button.
+Callback = <function> - The function of the button.
+]]
 
+--[[
+Name = <string> - The name of the button.
+Callback = <function> - The function of the button.
+]]
+local Tab = Window:MakeTab({
+	Name = "Fly Gui (Testing!)",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
 
+--[[
+Name = <string> - The name of the tab.
+Icon = <string> - The icon of the tab.
+PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
+]]
+local Section = Tab:AddSection({
+	Name = "Section"
+})
 
- local Button = MainTab:CreateButton({
-	Name = Fly Gui Testing'",
+--[[
+Name = <string> - The name of the section.
+]]OrionLib:MakeNotification({
+	Name = "Riley Hub",
+	Content = "Riley Hub Has Been Loaded",
+	Image = "rbxassetid://4483345998",
+	Time = 5
+})
+
+--[[
+Title = <string> - The title of the notification.
+Content = <string> - The content of the notification.
+Image = <string> - The icon of the notification.
+Time = <number> - The duration of the notfication.
+]]
+Tab:AddButton({
+	Name = "Fly Gui (Testing)",
 	Callback = function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
-	end,
- })
+      		print("button pressed")
+  	end    
+})
+
+--[[
+Name = <string> - The name of the button.
+Callback = <function> - The function of the button.
+]]
+Tab:AddButton({
+	Name = "Destroy Gui!",
+	Callback = function()
+     OrionLib:Destroy()
+      		print("button pressed")
+  	end    
+}) 
+
+--[[
+Name = <string> - The name of the button.
+Callback = <function> - The function of the button.
+]]
+Tab:AddButton({
+	Name = "Testing",
+	Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))();
+      		print("button pressed")
+  	end    
+})
+local Tab = Window:MakeTab({
+	Name = "Doors Script (Test)",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+--[[
+Name = <string> - The name of the tab.
+Icon = <string> - The icon of the tab.
+PremiumOnly = <bool> - Makes the tab accessible to Sirus Premium users only.
+]]
+Tab:AddButton({
+	Name = "Doors Hub (Testing)",
+	Callback = function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/zoophiliaphobic/POOPDOORS/main/script.lua'))()
+      		print("button pressed")
+  	end    
+})
+Tab:AddButton({
+	Name = "Auto A 1000 Geo (Testing)",
+	Callback = function()
+loadstring(game:HttpGet("https://" .. "raw.githubusercontent.com/DaRealGeo/roblox/master/rooms-autowalk"))()
+      		print("button pressed")
+  	end    
+})
+Tab:AddButton({
+	Name = "doors hub edited (Testing)",
+	Callback = function()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/poopdoors_edited/main/poopdoors_edited.lua"),true))()
+      		print("button pressed")
+  	end    
+})
+Tab:AddButton({
+	Name = "Auto A 1000 (Testing)",
+	Callback = function()
+loadstring(game:HttpGet("https://pastebin.com/raw/7Shu1JxY"))()
+      		print("button pressed")
+  	end    
+})
+OrionLib:Init()
